8535fc6cfa54452877eea4d4134e8ef4d669dde5
sky1o1 <aakash.dangol101@gmail.com>
2020-08-12 21:39:06 +0545

    [create] login models

--- club/views.py
+++ club/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet
 
 from .models import Club
@@ -14,10 +11,6 @@ class ClubViewSet(ViewSet):
     serializer_class = ClubSerializer
     queryset = Club.objects.all()
 
-    # def get(self, request, *args, **kwargs):
-    #     qs = club.objects.all()
-    #     serializer = clubserializer(instance=qs, many=true)
-    #     return response(serializer.data)
 
 
 
