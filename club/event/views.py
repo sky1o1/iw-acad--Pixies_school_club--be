c7a0c90757d64b2e71bcd52e091d4efef8b195d4
Megha Shrestha <meghashrestha30@gmail.com>
2020-08-20 20:31:36 +0545

    correction

--- club/event/views.py
+++ club/event/views.py
@@ -1,17 +1,16 @@
 from django.contrib.auth import get_user_model, authenticate, login
 from rest_framework import status, generics
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from rest_framework.views import APIView
-
 from .serializers import EventSerializer
-from club.models import UserStaffs, UserMembers, Club, Event
+from club.models import Event
 
 User = get_user_model()
-class EventSerializerView(CreateAPIView):
+class EventSerializerView(ListCreateAPIView):
+    queryset = Event.objects.all()
     serializer_class = EventSerializer
     authentication_classes = [TokenAuthentication, ]
     permission_classes = [AllowAny, ]
